bc3238cac39e7f935966c9386dfc35589908effd
actulity <75867049@qq.com>
2020-09-09 23:52:23 +0800

    137

---
+++ 137.只出现一次的数字-ii.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=137 lang=cpp
+ *
+ * [137] 只出现一次的数字 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int seenOnce = 0;
+        int seenTwice = 0;
+        for (int num : nums) 
+        {
+            seenOnce = ~seenTwice & (seenOnce ^ num);
+            seenTwice = ~seenOnce & (seenTwice ^ num);
+        }
+        return seenOnce;
+    }
+};
+// @lc code=end
+
